6a9b0d033c659ba3c22843b5db99d4c549602e35
hienbui9999 <buiduyhien@gmail.com>
2022-05-29 10:19:12 +0700

    Initial Commit

---
+++ ObjectiveCPackage_SampleCallToCasperSDK/ObjectiveCPackage_SampleCallToCasperSDK.h
@@ -0,0 +1,18 @@
+//
+//  ObjectiveCPackage_SampleCallToCasperSDK.h
+//  ObjectiveCPackage_SampleCallToCasperSDK
+//
+//  Created by Hien on 29/05/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ObjectiveCPackage_SampleCallToCasperSDK.
+FOUNDATION_EXPORT double ObjectiveCPackage_SampleCallToCasperSDKVersionNumber;
+
+//! Project version string for ObjectiveCPackage_SampleCallToCasperSDK.
+FOUNDATION_EXPORT const unsigned char ObjectiveCPackage_SampleCallToCasperSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObjectiveCPackage_SampleCallToCasperSDK/PublicHeader.h>
+
+
